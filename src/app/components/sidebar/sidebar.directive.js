569140f12ac134fdb4845a6eeceed7a8d3b4f7f4
Jesús Perales <jesus.peralesmr@uanl.edu.mx>
2016-03-17 18:52:51 -0600

    First commit

---
+++ src/app/components/sidebar/sidebar.directive.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('qrlite')
+    .directive('sidebar', sidebar);
+
+  /** @ngInject */
+  function sidebar() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/sidebar/sidebar.html',
+      scope: {
+          creationDate: '='
+      },
+      controller: SidebarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function SidebarController(moment) {
+      var vm = this;
+
+      // "vm.creation" is avaible by directive option "bindToController: true"
+      vm.relativeDate = moment(vm.creationDate).fromNow();
+    }
+  }
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('qrlite',
+    ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria',
+     'ngResource', 'ngRoute', 'ngMaterial', 'toastr', 'qrScanner']);
+
+})();
